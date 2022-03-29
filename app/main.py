29c192f5bdf0102883ee4161719c06763e116ba8
robert-gherlan <gherlanilierobert@gmail.com>
2022-03-29 14:33:16 +0300

    added CORS support

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .routers import post, user, root, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # Line used to generate the database based on defined models.
 # Commented because we are using "alembic".
@@ -7,8 +8,18 @@ from .routers import post, user, root, auth, vote
 
 app = FastAPI()
 
-app.include_router(post.router)
-app.include_router(user.router)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(root.router)
 app.include_router(auth.router)
+app.include_router(post.router)
+app.include_router(user.router)
 app.include_router(vote.router)
